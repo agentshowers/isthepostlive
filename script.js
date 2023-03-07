76ec4b4d813805c18ce4b86a15de41c6ca5e3f81
Joao Amorim <i.psyduck@gmail.com>
2023-03-07 12:53:07 +0000

    fix ugly indent

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 TODAY = (new Date()).setHours(0, 0, 0, 0);
 
 function parseDate(title) {
-	try {
-		var dateStart = title.indexOf("[") + 1;
-		var dateEnd = title.indexOf("]");
-		var dateText = title.slice(dateStart, dateEnd);
-		var split = dateText.split("/");
-		if (split.length != 3) {
-			throw "Invalid date";
-		}
-		var day = parseInt(split[0]);
-		var month = parseInt(split[1]) - 1;
-		var parsedYear = parseInt(split[2]);
-		var year = parsedYear > 2000 ? parsedYear : parsedYear + 2000;
-		return new Date(year, month, day);
-	} catch (err) {
-		console.log(`Failed to parse date from title "${title}"`);
-		return null;
-	}
+  try {
+    var dateStart = title.indexOf("[") + 1;
+    var dateEnd = title.indexOf("]");
+    var dateText = title.slice(dateStart, dateEnd);
+    var split = dateText.split("/");
+    if (split.length != 3) {
+      throw "Invalid date";
+    }
+    var day = parseInt(split[0]);
+    var month = parseInt(split[1]) - 1;
+    var parsedYear = parseInt(split[2]);
+    var year = parsedYear > 2000 ? parsedYear : parsedYear + 2000;
+    return new Date(year, month, day);
+  } catch (err) {
+    console.log(`Failed to parse date from title "${title}"`);
+    return null;
+  }
 }
 
 const App = {
